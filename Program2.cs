678101f42db99d9bd0a1614ee20dec42813412d0
Voldemar-imp <108013210+Voldemar-imp@users.noreply.github.com>
2022-07-04 19:57:49 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditionalOperatorsAndLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            bool areYouStaying = true;
+            string yourOrder;   
+            int numberOfOrders = 0;
+
+            Console.WriteLine("Добро пожаловать в бар!");
+            Console.WriteLine("Сделайте заказ официанту!");
+            Console.WriteLine("Или скажите exit, чтобы покинуть бар!");
+
+            while (areYouStaying)
+            {
+                Console.Write("К вам подходит официант и спашивет: чего жалаете? ");
+                yourOrder = Console.ReadLine();
+                if (yourOrder == "exit")
+                {
+                    areYouStaying = false;                    
+                    continue;
+                }
+                Console.WriteLine("Вам приносят: " + yourOrder);
+                numberOfOrders ++;
+            }                    
+            Console.WriteLine("Вы сделали " + numberOfOrders + " заказов.");
+            Console.WriteLine("Приходите ещё!");
+        }
+    }
+}
